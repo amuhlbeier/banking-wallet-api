364285ec8efb2d5bd8dcd4a90ed679d4b47f1b12
Alessandramuhlbeier <Alessandramuhlbeier84@gmail.com>
2025-03-17 13:53:53 -0400

    Added model and repository layers with Lombok

--- src/main/java/com/bankingapi/walletapi/model/User.java
+++ src/main/java/com/bankingapi/walletapi/model/User.java
@@ -1,10 +1,18 @@
 package com.bankingapi.walletapi.model;
 
 import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "users");
+@Table(name = "users")
 public class User {
 
     @Id
@@ -14,7 +22,7 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
@@ -23,37 +31,5 @@ public class User {
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    public User() {}
-
-    public User(String name, String email, String password, LocalDateTime createdAt) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.createdAt = createdAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
 }
+
