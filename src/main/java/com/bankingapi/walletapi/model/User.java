81ac118613c8a40581b187e8fd4b34ebfc4c9fd1
Alessandramuhlbeier <Alessandramuhlbeier84@gmail.com>
2025-03-17 13:23:23 -0400

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/bankingapi/walletapi/model/User.java
@@ -0,0 +1,59 @@
+package com.bankingapi.walletapi.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users");
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public User(String name, String email, String password, LocalDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
