17e6bff1aa85b2eb4d9058a5a51abb3cae138644
Alessandramuhlbeier <Alessandramuhlbeier84@gmail.com>
2025-03-29 12:08:45 -0400

    Added event types to listener class

--- src/main/java/com/bankingapi/walletapi/service/BankAccountEventListener.java
+++ src/main/java/com/bankingapi/walletapi/service/BankAccountEventListener.java
@@ -11,8 +11,38 @@ public class BankAccountEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(BankAccountEventListener.class);
 
-    @KafkaListener(topics = "bank-account-events", groupId = "bank-group", containerFactory = "bankAccountKafkaListenerFactory")
+    @KafkaListener(
+            topics = "bank-account-events",
+            groupId = "bank-group",
+            containerFactory = "bankAccountKafkaListenerFactory"
+    )
     public void handleBankAccountEvent(BankAccountEvent event) {
         logger.info("Received Kafka Event: {}", event);
+
+        switch (event.getEventType()) {
+            case "ACCOUNT_CREATED":
+                logger.info("Bank account created: {}", event.getAccountId());
+                break;
+            case "DEPOSIT":
+                logger.info("Deposit made to account {}: ${}", event.getAccountId(), event.getBalance());
+                break;
+            case "OVERDRAFT_EXCEEDED":
+                logger.info("Overdraft limit exceeded for account: {}", event.getAccountId());
+                break;
+            case "ACCOUNT_FROZEN":
+                logger.info("Account {} is frozen due to overdraft", event.getAccountId());
+                break;
+            case "WITHDRAWAL":
+                logger.info("Withdrawal made to account {}: ${}", event.getAccountId(), event.getBalance());
+                break;
+            case "MANUAL_FREEZE":
+                logger.info("Account {}  manually frozen", event.getAccountId());
+                break;
+            case "ACCOUNT_UNFROZEN":
+                logger.info("Account {} unfrozen", event.getAccountId());
+                break;
+            default:
+                logger.info("Received event: {}", event.getEventType());
+        }
     }
 }
