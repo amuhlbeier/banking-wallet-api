98365d91910cdbed9a5a814aeef0b18ede87b061
Ubuntu <ubuntu@ip-172-31-21-215.ec2.internal>
2025-04-04 01:23:12 +0000

    Added elastic IP to securityConfig

--- src/main/java/com/bankingapi/walletapi/security/SecurityConfig.java
+++ src/main/java/com/bankingapi/walletapi/security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost","http://44.220.157.108"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost","http://44.220.157.108","http://34.239.70.94"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
